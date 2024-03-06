1cbe093e39bb67b63228325c11db112fb5af50e8
DaniloSsoares <DaniloSsoares201@gmail.com>
2024-03-06 14:16:05 -0300

    Atividade de PW III

---
+++ Aula-3/resources/views/aula3.blade.php
@@ -0,0 +1,87 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Aula-3</title>
+</head>
+<body>
+    <div class="container">
+            <nav class="navbar navbar-expand-lg bg-primary ">
+                <div class="container-fluid bg-primary">
+                <a class="navbar-brand text-white " href="#">Sistema Web</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link active bg-white" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link bg-white" href="#">Cadastrar</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link bg-white" href="#">Consultar</a>
+                        </li>
+                     </ul>
+                </div>
+             </div>
+         </nav>
+         <p class="fs-1">Cadastrar -  Agendamento  de Potenciais Clientes</p>
+         <p class="fs-4">Sistema utilizado para agendamento de Serviços</p>
+         
+         
+         <table class="table table-bordered border-primary bg-primary" >
+             <thead>
+                   <tr>
+                        <th scope="col bg-primary bg-primary ">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">Bairro</th>
+                            <th scope="col">Cep</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">Ação</th>
+                        </tr>
+             </thead>
+                         <tbody>
+                               <tr>
+                                    <th scope="row">1</th>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                               </tr>
+                                    <tr>
+                                    <th scope="row">2</th>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                               </tr>
+                               <tr>
+                                    <th scope="row">3</th>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                               </tr>
+                                    
+                        </tbody>
+         </table>
+
+       </div>
+  </div>
+
+</body>
+</html>
